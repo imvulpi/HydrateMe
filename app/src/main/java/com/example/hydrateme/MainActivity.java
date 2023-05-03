206ce01aca3d3adc44e32d28bead6fe5ad3e5041
Pleysek <pjadasz07@gmail.com>
2023-05-04 01:33:14 +0200

    v0.1a | Water intake card
    Version 0.1a <- where 'a' means alpha
    
    1. Added the water intake card, which for now doesn't have usage (since this is just alpha)
    
    2. Changed ActionBar default color to main accent of the application.
    
    3. Changed default progressbar to custom .9.png progressbar that conveys this project, the progress is in blue (main accent) and the progressbar background is gray.
    
    4. Added other fonts.
    
    5. Added a card background, which has rounded corners. It is a background of water intake card.
    
    6. Added more colors, accents/main/text/normal,
    
    7. Changed the styles/themes so every theme is supported and looks acceptable.
    
    Summary: This was a productive update. Although this is really nice, it's just beginning of updates. I Mainly did: Organization

--- app/src/main/java/com/example/hydrateme/MainActivity.java
+++ app/src/main/java/com/example/hydrateme/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.hydrateme;
 
+import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,6 +14,8 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.hydrateme.databinding.ActivityMainBinding;
+
+import java.security.Security;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,9 +28,23 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Get the current UI mode
+        int uiMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+
+        // Set the action bar background color based on the UI mode
+        ActionBar actionBar = getSupportActionBar();
+        if (uiMode == Configuration.UI_MODE_NIGHT_YES) {
+            actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.main_accent_dark)));
+        } else {
+            actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.main_accent_light)));
+        }
+
+
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
